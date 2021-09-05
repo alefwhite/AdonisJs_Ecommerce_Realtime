ac38af4d18b7eaedb88a5438ab81a7c10bf947b4
alefwhite <alefwhite@hotmail.com>
2021-09-05 09:32:17 -0300

    Incluindo middlewares de auth e acl

--- start/routes/auth.js
+++ start/routes/auth.js
@@ -12,26 +12,32 @@ const Route = use('Route')
 Route.group(() => {
     Route.post('/register', 'AuthController.register')
       .as('auth.register')
+      .middleware(['guest'])
 
     Route.post('/login', 'AuthController.login')
       .as('auth.register')
+      .middleware(['guest'])
 
     Route.post('/refresh', 'AuthController.refresh')
       .as('auth.refresh')
+      .middleware(['guest'])
 
     Route.post('/logout', 'AuthController.logout')
       .as('auth.logout')
-
+      .middleware(['auth'])
 
     // Restore password routes
     Route.post('/reset-password', 'AuthController.forgot')
       .as('auth.forgot')
+      .middleware(['guest'])
 
     Route.get('/reset-password', 'AuthController.remember')
       .as('auth.remember')
+      .middleware(['guest'])
 
     Route.put('/reset-password', 'AuthController.reset')
       .as('auth.reset')
+      .middleware(['guest'])
 })
 .prefix('v1/auth')
 .namespace('Auth')
